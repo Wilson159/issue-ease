3790c3ced4004345b5b032c8ecf7e2fc6a1ce9ad
Wilson <megalegendsserver@gmail.com>
2024-05-27 20:00:37 +0100

    CUID Primary Key - Users

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,13 @@
-import { Table, Column, Model, DataType } from 'sequelize-typescript';
+import { Table, Column, Model, DataType, PrimaryKey, Default } from 'sequelize-typescript';
+const cuid = require('cuid');
 
 @Table
 export class User extends Model<User> {
+    @PrimaryKey
+    @Default(cuid())
+    @Column
+    id: string;
+
     @Column({
         type: DataType.STRING,
         allowNull: false,
